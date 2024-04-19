6c4dedc93ef8c0d47feb3fad859acf84691d0945
Mounir <82580725+mounirnader@users.noreply.github.com>
2024-04-19 23:11:26 +0200

    validation

--- public/js/account.js
+++ public/js/account.js
@@ -1,11 +1,53 @@
-const signupLogin = document.getElementById('signup-login')
-const more = document.getElementById('more')
+const dummy = {
+    'name': 'Mounir Nader',
+    'email': 'mony@bookking.com',
+    'dateOfBirth': '2003-10-21',
+    'password': '12345678',
+    'phone': '01000000000',
+    'city': 'Cairo',
+    'address': '185 el orouba',
+    'role': 'user'
+}
+const admin = {
+    'email': 'admin@bookking.com',
+    'name': 'Book King',
+    'password': 'bookking',
+    'role': 'admin'
+}
+const users = [dummy, admin];
 
-more.addEventListener("click", function() {    
-    if (signupLogin.style.display === "none") {
-        signupLogin.style.display = "block";
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("passw");
+
+
+function validatePassword() {
+    if (passwordInput.value.length < 8) {
+        document.getElementById('error-4').style.display = 'block';
+        return false;
     } else {
-        signupLogin.style.display = "none";
+        document.getElementById('error-4').style.display = 'none';
+        return true
     }
-});
+}
+passwordInput.addEventListener("input", validatePassword);
+function logIn() {
+    const email = emailInput.value.trim();
+    const password = passwordInput.value.trim();
+
+        const user = users.find(function(user) {
+            return user.email === email && user.password === password;
+        });
+        if (user && validatePassword()) {
+            document.getElementById('error-6').style.display = 'none';
+          if(user.role === 'admin') {
+            location.replace('dashboard.html')
+          }
+          else location.replace(`account.html?email=${email}&name=${user.name}&phone=${user.phone}&city=${user.city}&address=${user.address}&dateOfBirth=${user.dateOfBirth}`)
+        } else {
+          document.getElementById('error-6').style.display = 'block';
+        }
+    }
+
+document.getElementById("login").addEventListener("click", logIn);
+
 
